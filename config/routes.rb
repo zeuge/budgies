cda5b6636754611f182e4b2a58d27d498e99fced
Eugene Zhukov <zeuge74@gmail.com>
2015-12-26 12:54:18 +0500

    add controller "Main"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  root "main#index"
+  get "main/index"
+
   resources :budgies, except: [:new, :edit], :defaults => { :format => :json }
   resources :colors, except: [:new, :edit], :defaults => { :format => :json }
 
